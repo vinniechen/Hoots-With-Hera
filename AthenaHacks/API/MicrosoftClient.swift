c58bb749879f63d09bdbbaeb4cfd98e1cb1550d0
Vinnie Chen <vinnie.chen211@gmail.com>
2018-02-24 20:32:55 -0800

    First Commit

---
+++ AthenaHacks/API/MicrosoftClient.swift
@@ -0,0 +1,14 @@
+//
+//  MicrosoftClient.swift
+//  AthenaHacks
+//
+//  Created by Vinnie Chen on 2/24/18.
+//  Copyright © 2018 Vinnie Chen. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class MicrosoftClient: BDBOAuth1SessionManager {
+
+}
